331e8c17373ad1280c75fe717495f6dbc3edc5e6
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2022-08-30 10:38:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,32 @@ full_name.setAttribute('type', 'text');
 full_name.setAttribute('id', 'full_name');
 full_name.setAttribute('placeholder', 'Full Name');
 document.body.appendChild(full_name);
-var email = document.createElement('input');
-email.setAttribute('type', 'text');
-email.setAttribute('id', 'email');
-email.setAttribute('placeholder', 'Email');
-document.body.appendChild(email);
 var phone = document.createElement('input');
 phone.setAttribute('type', 'text');
 phone.setAttribute('id', 'phone');
 phone.setAttribute('placeholder', 'Phone');
 document.body.appendChild(phone);
+var email = document.createElement('input');
+email.setAttribute('type', 'text');
+email.setAttribute('id', 'email');
+email.setAttribute('placeholder', 'Email');
+document.body.appendChild(email);
 var submit = document.createElement('button');
 submit.setAttribute('type', 'button');
 submit.setAttribute('id', 'submit');
 submit.innerHTML = 'Submit';
 document.body.appendChild(submit);
+var json = document.createElement('div');
+json.setAttribute('id', 'json');
+document.body.appendChild(json);
+submit.onclick = function() {
+  var full_name_value = document.getElementById('full_name').value;
+  var phone_value = document.getElementById('phone').value;
+  var email_value = document.getElementById('email').value;
+  var json_value = {
+    full_name: full_name_value,
+    phone: phone_value,
+    email: email_value
+  };
+  document.getElementById('json').innerHTML = JSON.stringify(json_value);
+};
