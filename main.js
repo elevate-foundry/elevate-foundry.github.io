45e5a6cea2d68d3a03f885c6a3efdabd570f3cfd
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2022-08-30 17:10:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-/* Create "Generate identity verification link" button that on submit makes an API post to url "https://realquickapp.stradum-dev.io/api/v1/transactions" and if transaction is successful, displays the JSON response below the button. */
-var button = document.createElement('button');
-button.innerHTML = 'Generate identity verification link';
-document.body.appendChild(button);
-button.addEventListener('click', function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://realquickapp.stradum-dev.io/api/v1/transactions', true);
-//   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var response = document.createElement('div');
-      response.innerHTML = xhr.responseText;
-      document.body.appendChild(response);
-    }
-  };
-//   xhr.send(JSON.stringify({
-//     "transaction": {
-//       "transaction_type": "identity_verification",
-//       "transaction_data": {
-//         "first_name": "John",
-//         "last_name": "Doe",
-//         "email": "john.doe@example.com",
-//         "phone": "555-555-5555",
-//         "dob": "01/01/1980",
-//         "ssn": "123-45-6789",
-//         "address": "123 Main St",
-//         "city": "New York",
-//         "state": "NY",
-//         "zip": "10001"
-//       }
-//     }
-  }));
-});
+function postData(input) {
+    $.ajax({
+        type: "POST",
+        url: "/main.py",
+        data: { param: input },
+        success: callbackFunc
+    });
+}
+
+function callbackFunc(response) {
+    // do something with the response
+    console.log(response);
+}
+
+postData('data to process');
