08fccc6ebf6f90e2d25d1d6728930cc951efa561
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2022-08-30 20:23:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-// Get the button and container elements from HTML:
+// Get the button and container from HTML:
 const button = document.getElementById("theButton")
 const data = document.getElementById("info")
-// Create an array of cars to send to the server:
-const cars = [
-	{ "make":"Porsche", "model":"911S" },
-	{ "make":"Mercedes-Benz", "model":"220SE" },
-	{ "make":"Jaguar","model": "Mark VII" }
-];
 // Create an event listener on the button element:
 button.onclick= function(){
-    // Get the reciever endpoint from Python using fetch:
+    
+    // Get the receiver endpoint from Python using fetch:
     fetch("http://127.0.0.1:5000/receiver", 
         {
             method: 'POST',
@@ -26,9 +21,9 @@ button.onclick= function(){
                 }
             }).then(jsonResponse=>{
                 
-                // Log the response data in the console
-                console.log(jsonResponse)
-            } 
-            ).catch((err) => console.error(err));
-            
-           }
+                // Iterate through the data with Map and write your rendering logic:
+                jsonResponse.map(Main=>            
+       Main.make==="Porsche"? data.innerHTML +="<p>"+ Main.make+" "+" is a good product":
+      data.innerHTML +="<p>"+ Main.make+" "+"is an average product" ) 
+} 
+).catch((err) => console.error(err)); } 
