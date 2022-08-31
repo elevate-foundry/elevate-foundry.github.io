52a73f64a16ea43a73e8921d4e760a33bc1dc8fb
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2022-08-30 20:04:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 <strong>#Set up Flaskstrong>:
 app = Flask(__name__)
-#Set up Flask to bypass CORS at the front end:
+<strong>#Set up Flask to bypass CORSstrong>:
 cors = CORS(app)
-#Run the app:
-if __name__ == "__main__":
-     app.run()
+#Create the receiver API POST endpoint:
+@app.route("/receiver", methods=["POST"])
+def postME():
+   data = request.get_json()
+   data = jsonify(data)
+   return data
+if __name__ == "__main__": 
+   app.run(debug=True)
