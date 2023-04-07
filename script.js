55d48abbb5d30c9bc66fb12d2a3f0beb9a2370eb
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2023-04-06 20:32:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const csvFileInput = document.getElementById('csvFile');
 const uploadButton = document.getElementById('uploadButton');
+const spinner = document.getElementById('spinner');
+const message = document.getElementById('message');
 
 // Load the Google Cloud Storage client library
 gapi.load('client', initGCSClient);
 
 async function initGCSClient() {
   // Replace with the path to your JSON key file
-  const keyFile = 'elevate-foundry-280fa96289f8.json';
+  const keyFile = '/path/to/your-key-file.json';
 
   // Load the JSON key file
   const response = await fetch(keyFile);
@@ -44,6 +46,10 @@ function uploadToGCS() {
     reader.onload = function (event) {
       const fileData = event.target.result.split(',')[1];
 
+      // Show the spinner
+      spinner.style.display = 'block';
+      message.textContent = '';
+
       // Upload the file to Google Cloud Storage
       gapi.client.request({
         'path': `https://storage.googleapis.com/upload/storage/v1/b/${bucketName}/o`,
@@ -58,8 +64,14 @@ function uploadToGCS() {
         },
         'body': fileData,
       }).then((response) => {
+        // Hide the spinner and show a success message
+        spinner.style.display = 'none';
+        message.textContent = 'Successfully uploaded the CSV file.';
         console.log('Successfully uploaded the CSV file:', response);
       }, (error) => {
+        // Hide the spinner and show an error message
+        spinner.style.display = 'none';
+        message.textContent = 'Error uploading the CSV file: ' + error.message;
         console.error('Error uploading the CSV file:', error);
       });
     };
