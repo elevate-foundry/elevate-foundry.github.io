86d319e9aeed1cd251059eec6694c7068d1675a2
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2023-04-06 20:46:29 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+const {Storage} = require('@google-cloud/storage');
+
+const app = express();
+const upload = multer({storage: multer.memoryStorage()});
+const storage = new Storage({
+  projectId: '<your_project_id>',
+  credentials: JSON.parse(process.env.GCS_KEY_FILE_CONTENTS),
+});
+
+const bucketName = '<your-bucket-name>';
+
+app.use(express.static(__dirname));
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+  const file = req.file;
+
+  if (file && file.mimetype === 'text/csv') {
+    const blob = storage.bucket(bucketName).file(file.originalname);
+    const blobStream = blob.createWriteStream({resumable: false});
+
+    blobStream.on('error', (error) => {
+      res.status(500).send('Error uploading the CSV file: ' + error.message);
+    });
+
+    blobStream.on('finish', () => {
+      res.status(200).send('Successfully uploaded the CSV file.');
+    });
+
+    blobStream.end(file.buffer);
+  } else {
+    res.status(400).send('Please select a valid CSV file.');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
