9181101bf30a865fb685e1872925965f0854c158
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2023-04-06 19:50:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,18 @@ const s3 = new AWS.S3({
 // Get DOM elements
 const csvFileInput = document.getElementById('csvFile');
 const uploadButton = document.getElementById('uploadButton');
+const spinner = document.getElementById('spinner');
+const message = document.getElementById('message');
 
 // Add a click event listener to the upload button
 uploadButton.addEventListener('click', function () {
   const file = csvFileInput.files[0];
 
   if (file && file.type === 'text/csv') {
+    // Show the spinner
+    spinner.style.display = 'block';
+    message.textContent = '';
+
     // Upload the file to the S3 bucket
     s3.upload(
       {
@@ -29,10 +35,17 @@ uploadButton.addEventListener('click', function () {
         ACL: 'public-read', // Set the access level of the uploaded file, adjust as needed
       },
       function (err, data) {
+        // Hide the spinner
+        spinner.style.display = 'none';
+
         if (err) {
           console.error('Error uploading the CSV file:', err);
+          message.textContent = 'Error uploading the CSV file: ' + err.code;
+          message.style.color = 'red';
         } else {
           console.log('Successfully uploaded the CSV file:', data);
+          message.textContent = 'Successfully uploaded the CSV file.';
+          message.style.color = 'green';
         }
       }
     );
